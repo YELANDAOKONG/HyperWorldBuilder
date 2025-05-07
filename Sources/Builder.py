07bfbba41a6307861bab318d3e2caa075ac67fb7
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-05-08 02:16:56 +0800

    创建文件

---
+++ Sources/Builder.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import json
+import tomllib
+
+class Builder:
+    def __init__(self, path):
+        self.path = path
+
+    # TODO...
---
+++ Sources/Main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+
+def main(args: List[str]) -> int:
+    print("TODO...")
+    # TODO...
+    return 0
